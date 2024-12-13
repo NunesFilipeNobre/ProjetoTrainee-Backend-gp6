fdef475dbd48705c78ed441d63bb79caab286cc0
Filipe Nunes <nunesfilipenobre@gmail.com>
2024-12-12 21:36:36 -0300

    hashing de senhas

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,48 +1,118 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UserService {
   constructor(private readonly prisma: PrismaService) {}
 
   async create(data: CreateUserDto) {
-    const user = await this.prisma.user.create({
-      data: data,
+    const existingUser = await this.prisma.user.findUnique({
+      where:{email: data.email}});
+
+      if (existingUser){
+        throw new ConflictException('User with this email already exists');
+      }
+
+      const hashedPassword = await bcrypt.hash(data.senha,10);
+
+    return await this.prisma.user.create({
+      data: {...data, senha: hashedPassword}
     });
-    return user;
   }
 
   async findAll() {
-    const usuarios = await this.prisma.user.findMany({});
-    console.log(usuarios)
-    return usuarios
+    return await this.prisma.user.findMany({
+      select:{
+        id:true,
+        nome:true,
+        email:true,
+        departamento:true,
+        curso:true,
+        Avaliacoes:true,
+        Comentarios:true,
+        createdAt:true,
+        updatedAt:true
+      },
+    });
   }
 
   async findOne(id: number) {
-    const usuario = await this.prisma.user.findUnique({
-      where: {
-        id: id,
+    const isValidId = await this.prisma.user.findUnique({where:{id}})
+    if(!isValidId){
+      throw new NotFoundException('User not Found');
+    }
+    return await this.prisma.user.findUnique({where:{id},
+      select:{
+        id:true,
+        nome:true,
+        email:true,
+        departamento:true,
+        curso:true,
+        Avaliacoes:true,
+        Comentarios:true,
+        createdAt:true,
+        updatedAt:true
       },
     });
-    return usuario
   }
 
   async del_user(id: number) {
-    return await this.prisma.user.delete({
-      where: {
-        id: id,
+    const isValidId = await this.prisma.user.findUnique({where: {id}});
+    if(!isValidId){
+      throw new NotFoundException('User not found');
+    }
+
+
+    return await this.prisma.user.delete({where: { id },
+      select:{
+        id:true,
+        nome:true,
+        email:true,
+        departamento:true,
+        curso:true,
+        Avaliacoes:true,
+        Comentarios:true,
+        createdAt:true,
+        updatedAt:true
       },
     });
   }
 
+  async findByEmail(email:string){
+    const user = await this.prisma.user.findUnique({where: {email}});
+    if(!user){
+      return null;
+    }
+
+    return user;
+  }
+
   async update_user(id: number, data: UpdateUserDto) {
+    const isValidId = await this.prisma.user.findUnique({where: {id}});
+    if(!isValidId){
+      throw new NotFoundException('User not found');
+    }
+
+    const hashedPassword = await bcrypt.hash(data.senha,10);
     return await this.prisma.user.update({
-      where: {
-        id: id,
-      },
-      data: data,
+      where: {id},
+      data: {...data,
+      senha: hashedPassword
+    },
+    select:{
+      id:true,
+      nome:true,
+      email:true,
+      departamento:true,
+      curso:true,
+      Avaliacoes:true,
+      Comentarios:true,
+      createdAt:true,
+      updatedAt:true
+    },
     });
   }
 }
